f7420aefc1fa4b89b6ac025bb020c2bc03ec0a6e
Enzo Cazenave <encaze24@hotmail.com>
2024-01-10 13:40:12 -0300

    Agrega secciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'yellow-cubicd': '#EDFF53',
+      },
+      lineHeight: {
+        '28': '1.75rem',
+        '58': '3.625rem',
+        '72': '4.5rem'
+      }
+    },
+    fontFamily: {
+      sans: ['Helvetica', 'sans-serif']
+    }
   },
   plugins: [],
 }
